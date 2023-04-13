04b69d116b4d22f4f1fe8dbcd1c3c73b0feac3e2
santu patra <santupatra02@gmail.com>
2023-04-13 14:52:43 +0530

    initial commit

---
+++ src/main/java/com/patra/santu/MergeYourApisApplication.java
@@ -0,0 +1,13 @@
+package com.patra.santu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergeYourApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergeYourApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/patra/santu/MergeYourApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patra.santu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergeYourApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
